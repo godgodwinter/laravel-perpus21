a230542e4832f492739f5ebcc5866247e824dcb2
kakadlz <chibi.m4ru@gmail.com>
2021-09-18 01:48:52 +0700

    update layout admin

--- resources/views/layouts/layout1.blade.php
+++ resources/views/layouts/layout1.blade.php
@@ -23,47 +23,46 @@
         <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
-        <a href="../assets/index3.html" class="nav-link">Home</a>
+        <a href="../assets/index3.html" class="nav-link">Pinjam</a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
-        <a href="#" class="nav-link">Contact</a>
+        <a href="#" class="nav-link">Kembalikan</a>
+      </li>
+      <li class="nav-item d-none d-sm-inline-block">
+        <a href="#" class="nav-link">Periksa Buku</a>
       </li>
     </ul>
 
     <!-- Right navbar links -->
     <ul class="navbar-nav ml-auto">
 
-      <!-- Notifications Dropdown Menu -->
-      <li class="nav-item dropdown">
-        <a class="nav-link" data-toggle="dropdown" href="#">
-          <i class="far fa-bell"></i>
-          <span class="badge badge-warning navbar-badge">15</span>
-        </a>
-        <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-          <span class="dropdown-item dropdown-header">15 Notifications</span>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item">
-            <i class="fas fa-envelope mr-2"></i> 4 new messages
-            <span class="float-right text-muted text-sm">3 mins</span>
-          </a>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item">
-            <i class="fas fa-users mr-2"></i> 8 friend requests
-            <span class="float-right text-muted text-sm">12 hours</span>
-          </a>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item">
-            <i class="fas fa-file mr-2"></i> 3 new reports
-            <span class="float-right text-muted text-sm">2 days</span>
-          </a>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
-        </div>
-      </li>
       <li class="nav-item">
         <a class="nav-link" data-widget="fullscreen" href="#" role="button">
           <i class="fas fa-expand-arrows-alt"></i>
         </a>
+      </li>
+      <!-- Notifications Dropdown Menu -->
+      <li class="nav-item dropdown">
+        <a class="nav-link" data-toggle="dropdown" href="#">
+          <i class="fas fa-cog"></i>
+          <span class="badge badge-warning navbar-badge">3</span>
+        </a>
+        <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+          <div class="dropdown-divider"></div>
+          <a href="#" class="dropdown-item">
+            <i class="fas fa-user-cog"></i> Profil
+          </a>
+          <div class="dropdown-divider"></div>
+          <a href="#" class="dropdown-item">
+            <i class="fas fa-cogs"></i> Pengaturan
+          </a>
+          <div class="dropdown-divider"></div>
+          <div class="dropdown-divider"></div>
+          <a href="#" class="dropdown-item">
+            <i class="fas fa-sign-out-alt"></i> <span class=" text-muted text-sm">Logout</span> 
+           
+          </a>
+        </div>
       </li>
     </ul>
   </nav>
@@ -137,63 +136,63 @@
           <li class="nav-header">MASTER</li>
           <li class="nav-item">
           <a href="#" class="nav-link">
-            <i class="nav-icon far fa-circle text-info"></i>
+            <i class="fas fa-box-open"></i>
             <p>Rak Buku</p>
           </a>
         </li>
           <li class="nav-item">
           <a href="#" class="nav-link">
-            <i class="nav-icon far fa-circle text-info"></i>
+            <i class="fas fa-archive"></i>
             <p>Kategori</p>
           </a>
         </li>
         <li class="nav-item">
           <a href="#" class="nav-link">
-            <i class="nav-icon far fa-circle text-info"></i>
+            <i class="fas fa-book"></i>
             <p>Buku</p>
           </a>
         </li>
           <li class="nav-item">
             <a href="#" class="nav-link">
-              <i class="nav-icon far fa-circle text-info"></i>
+              <i class="far fa-user"></i>
               <p>Pengunjung</p>
             </a>
           </li>
           <li class="nav-item">
           <a href="#" class="nav-link">
-            <i class="nav-icon far fa-circle text-info"></i>
+            <i class="fas fa-user-tie"></i>
             <p>Anggota</p>
           </a>
         </li>
         <li class="nav-item">
         <a href="#" class="nav-link">
-          <i class="nav-icon far fa-circle text-info"></i>
+          <i class="fas fa-screwdriver"></i>
           <p>Peralatan</p>
         </a>
       </li>
           <li class="nav-header">PROSES</li>
           <li class="nav-item">
             <a href="#" class="nav-link">
-              <i class="nav-icon far fa-circle text-info"></i>
+              <i class="fas fa-calendar-plus"></i>
               <p>Peminjaman</p>
             </a>
           </li>
           <li class="nav-item">
             <a href="#" class="nav-link">
-              <i class="nav-icon far fa-circle text-info"></i>
+              <i class="far fa-calendar-check"></i>
               <p>Pengembalian</p>
             </a>
           </li>
           <li class="nav-header">REPORT</li>
           <li class="nav-item">
             <a href="#" class="nav-link">
-              <i class="nav-icon far fa-circle text-info"></i>
+              <i class="fas fa-users"></i>
               <p>Laporan Pengunjung</p>
             </a>
           </li>
           <li class="nav-item">
             <a href="#" class="nav-link">
-              <i class="nav-icon far fa-circle text-info"></i>
+              <i class="fas fa-user-clock"></i>
               <p>Laporan Peminjaman</p>
             </a>
           </li>
